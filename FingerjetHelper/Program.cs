0ea9f23dadfa244abb8d38d842fd79bb92abb4bb
Iam Navs <jcarlounavas@gmail.com>
2026-04-16 12:16:47 +0800

    Fixing route

--- FingerjetHelper/Program.cs
+++ FingerjetHelper/Program.cs
@@ -10,24 +10,27 @@ namespace FingerjetHelper
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
             builder.Services.AddControllers();
 
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            app.MapOpenApi();
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
             {
-                app.MapOpenApi();
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
+            app.MapGet("/", context => 
+            {
+                context.Response.Redirect("/swagger/index.html");
+                return System.Threading.Tasks.Task.CompletedTask;
+            });
 
             app.UseHttpsRedirection();
 
